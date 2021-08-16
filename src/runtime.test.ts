6e5b47c6978e41cd3020df22e699e4fe3d9ef81c
Delon Newman <contact@delonnewman.name>
2021-08-16 16:59:33 -0600

    adding meta types

--- src/runtime.test.ts
+++ src/runtime.test.ts
@@ -1,15 +1,61 @@
-import * as tsr from "./runtime"
+import tsr from "./runtime"
 
 test('any will always return true', () => {
-  expect(tsr.any()(1)).toBe(true)
+  expect(tsr.any.satisfies(tsr.any.generate())).toBe(true)
 })
 
 test('object will return true if the value is an object', () => {
-  let isObj = tsr.object()
-  expect(isObj(1)).toBe(false)
-  expect(isObj("")).toBe(false)
-  expect(isObj([])).toBe(true)
-  expect(isObj({})).toBe(true)
-  expect(isObj(function() { })).toBe(true)
-  expect(isObj(new String(""))).toBe(true)
+  const failing = [
+    { value: 1, result: false },
+    { value: "", result: false },
+    { value: [], result: false },
+    { value: function() { }, result: false },
+    { value: new String(""), result: false }
+  ]
+
+  let obj = tsr.object
+  failing.forEach((example) => {
+    expect(obj.satisfies(example.value)).toBe(example.result)
+  })
+  expect(obj.satisfies(obj.generate())).toBe(true)
+})
+
+test('number will return true if the value is a number', () => {
+  let num = tsr.number
+
+  expect(num.satisfies(num.generate())).toBe(true)
+  expect(num.satisfies("")).toBe(false)
+})
+
+test('string will return true if the value is a string', () => {
+  let str = tsr.string
+  expect(str.satisfies(1)).toBe(false)
+  expect(str.satisfies(str.generate())).toBe(true)
+})
+
+test('boolean will return true if the value is true or false', () => {
+  let bool = tsr.boolean
+  expect(bool.satisfies(bool.generate())).toBe(true)
+  expect(bool.satisfies(2)).toBe(false)
+})
+
+test('null will return true if the value is null', () => {
+  let nullType = tsr.null
+  expect(nullType.satisfies(null)).toBe(true)
+  expect(nullType.satisfies(undefined)).toBe(false)
+  expect(nullType.satisfies(1)).toBe(false)
+})
+
+test('undefined will return true if the value is null', () => {
+  let undef = tsr.undefined
+  expect(undef.satisfies(null)).toBe(false)
+  expect(undef.satisfies(undefined)).toBe(true)
+  expect(undef.satisfies(1)).toBe(false)
+})
+
+test("array will return true if the value is an array and it's elements match the extention type", () => {
+  let array = tsr.Array
+  expect(array.satisfies(tsr.any, [1, "2", 3])).toBe(true)
+  expect(array.satisfies(tsr.number, [1, "2", 3])).toBe(false)
+  expect(array.satisfies(tsr.number, [1, 2, 3])).toBe(true)
 })
--- src/runtime.ts
+++ src/runtime.ts
@@ -1,7 +1,114 @@
-export function any() {
-  return (_: any) => true
+type SimpleTypePredicate = (value: any) => boolean
+type GenericTypePredicate = (extType: TypePredicate) => TypePredicate
+type TypePredicate =
+  | SimpleTypePredicate
+  | GenericTypePredicate
+
+interface MetaType<T> {
+  getName(): string
+  getPredicate(): TypePredicate
+  generate(...args): T
+  satisfies(value: any, other?: any): boolean
 }
 
-export function object() {
-  return (value: any) => isObject(value)
+
+class SimpleMetaType<T> implements MetaType<T> {
+  private name: string
+  private predicate: SimpleTypePredicate
+  private generator: () => T
+
+  constructor(name: string, predicate: SimpleTypePredicate, generator: () => T) {
+    this.name = name
+    this.predicate = predicate
+    this.generator = generator
+  }
+
+  generate(): T {
+    return this.generator()
+  }
+
+  satisfies(value: any): boolean {
+    return this.predicate(value)
+  }
+
+  getName(): string {
+    return this.name
+  }
+
+  getPredicate(): SimpleTypePredicate {
+    return this.predicate
+  }
 }
+
+class GenericMetaType<T> implements MetaType<T> {
+  private name: string
+  private predicate: GenericTypePredicate
+  private generator: (extGen: () => any) => T
+
+  constructor(name: string, predicate: GenericTypePredicate, generator: () => T) {
+    this.name = name
+    this.predicate = predicate
+    this.generator = generator
+  }
+
+  generate(extGen: () => any): T {
+    return this.generator(extGen)
+  }
+
+  satisfies(value: any, extType: MetaType<any>): boolean {
+    // TODO: will have to unravel the predicate
+    if (extType instanceof SimpleMetaType)
+      return this.predicate(extType.getPredicate())(value)
+  }
+
+  getName(): string {
+    return this.name
+  }
+
+  getPredicate(): GenericTypePredicate {
+    return this.predicate
+  }
+}
+
+const objectTypePredicate = (type: string): SimpleTypePredicate => (
+  (value: any) => Object.prototype.toString.call(value) === `[object ${type}]`
+)
+
+const genericTypePredicate = (baseType: SimpleTypePredicate, f: (value: any, extType: TypePredicate) => boolean): GenericTypePredicate =>
+  (extType: TypePredicate): TypePredicate => (value: any) => baseType(value) && f(value, extType)
+
+function objectType<T>(type: string, generator: () => T): SimpleMetaType<T> {
+  return new SimpleMetaType<T>(type.toLowerCase(), objectTypePredicate(type), generator)
+}
+
+const AnyType = new SimpleMetaType<any>('any', (_: any) => true, () => 1)
+const ObjectType = objectType('Object', () => ({}))
+const NumberType = objectType('Number', () => 1)
+const StringType = objectType('String', () => "")
+const BooleanType = objectType('Boolean', () => true)
+const NullType = new SimpleMetaType<null>('null', (value: any) => value === null, () => null)
+const UndefinedType = new SimpleMetaType<undefined>('undefined', (value: any) => value === void 0, () => undefined)
+
+const isArray = objectTypePredicate('Array')
+
+const genericArray: GenericTypePredicate = genericTypePredicate(isArray, (array, extType) =>
+  array.reduce((res: boolean, val: any) => res && extType(val), true))
+
+const ArrayType = new GenericMetaType<Array<any>>('Array<T>', genericArray, () => [])
+
+const isFunction = objectTypePredicate('Function')
+
+const genericFunction: GenericTypePredicate = genericTypePredicate(isFunction, (f, extType) => true)
+
+const tsr = {
+  any: AnyType,
+  object: ObjectType,
+  number: NumberType,
+  string: StringType,
+  boolean: BooleanType,
+  null: NullType,
+  undefined: UndefinedType,
+  Array: ArrayType,
+}
+
+export default tsr
