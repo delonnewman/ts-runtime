230e79b71786129c2d53dd1032050999727d9cd6
Delon Newman <contact@delonnewman.name>
2021-09-05 20:18:39 -0600

    WIP: Almost have one transformation working

--- src/parser.test.ts
+++ src/parser.test.ts
@@ -1,1 +1,16 @@
-import parser from "./parser"
+import * as tsr from "./parser"
+
+test('it takes a type ast node an converts it to an equivalent meta object', () => {
+  const ast = tsr.transform(tsr.parse('type Person = { name: string, age: number }'))
+  expect(ast.type).toBe('Program')
+  expect(ast.body[0].type).toBe('VariableDeclaration')
+  expect(ast.body[0].kind).toBe('const')
+  expect(ast.body[0]).toHaveProperty('declarations')
+  expect(ast.body[0].declarations[0].id.name).toBe('Person')
+  expect(ast.body[0].declarations[0].init.type).toBe('CallExpression')
+  expect(ast.body[0].declarations[0].init.callee.type).toBe('MemberExpression')
+  expect(ast.body[0].declarations[0].init.callee.object.name).toBe('TSRuntime')
+  expect(ast.body[0].declarations[0].init.callee.property.name).toBe('Object')
+  expect(ast.body[0].declarations[0].init.arguments[0].type).toBe('CallExpression')
+  expect(ast.body[0].declarations[0].init.arguments[0].callee.type).toBe('MemberExpression')
+})
--- src/parser.ts
+++ src/parser.ts
@@ -3,3 +3,79 @@ import * as ts from "@typescript-eslint/typescript-estree"
 export function parse(code: string) {
   return ts.parse(code)
 }
+
+
+export function transform(ast) {
+  switch (ast.type) {
+    case 'Program':
+      return transformProgram(ast)
+    case 'TSTypeAliasDeclaration':
+      return transformTypeAlias(ast)
+    case 'TSTypeLiteral':
+      return transformTypeLiteral(ast)
+    case 'TSPropertySignature':
+      return transformPropertySignature(ast)
+    case 'TSTypeAnnotation':
+      return transformTypeAnnotation(ast)
+    case 'TSStringKeyword':
+      return 'string'
+    case 'TSNumberKeyword':
+      return 'number'
+    default:
+      throw new Error(`Unknown ast type: ${ast.type}`)
+  }
+}
+
+function transformProgram(ast) {
+  const node = Object.assign({}, ast)
+  node.body = ast.body.map(transform)
+  return node
+}
+
+function transformTypeAlias(ast) {
+  return {
+    type: 'VariableDeclaration',
+    kind: 'const',
+    declarations: [{ id: ast.id, init: transform(ast.typeAnnotation) }],
+  }
+}
+
+function transformTypeLiteral(ast) {
+  return {
+    type: 'CallExpression',
+    callee: {
+      type: 'MemberExpression',
+      object: { type: 'Identifier', name: 'TSRuntime' },
+      property: { type: 'Identifier', name: 'Object' },
+    },
+    arguments: ast.members.map(transform)
+  }
+}
+
+function transformPropertySignature(ast) {
+  return {
+    type: 'CallExpression',
+    callee: {
+      type: 'MemberExpression',
+      object: { type: 'Identifier', name: 'TSRuntime' },
+      property: { type: 'Identifier', name: 'ObjectProperty' },
+      compututed: false,
+      optional: false,
+    },
+    arguments: [
+      { type: 'Literal', value: ast.key.name, raw: JSON.stringify(ast.key.name) },
+      transform(ast.typeAnnotation),
+    ],
+    optional: false,
+  }
+}
+
+function transformTypeAnnotation(ast) {
+  return {
+    type: 'MemberExpression',
+    object: { type: 'Identifier', name: 'TSRuntime' },
+    property: transform(ast.typeAnnotation),
+    computed: false,
+    optional: false,
+  }
+}
