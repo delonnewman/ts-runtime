fe87a16703b273e2cd1d4b09ff0cfea5af7e5c8e
Delon Newman <contact@delonnewman.name>
2021-09-05 22:08:30 -0600

    Transforming object literal type aliases is working

--- src/parser.ts
+++ src/parser.ts
@@ -4,31 +4,56 @@ export function parse(code: string) {
   return ts.parse(code)
 }
 
+const BASIC_TYPES = {
+  TSStringKeyword: 'string',
+  TSNumberKeyword: 'number',
+  TSBooleanKeyword: 'boolean',
+  TSNullKeyword: 'null',
+  TSUndefinedKeyword: 'undefined',
+  TSAnyKeyword: 'any',
+  TSObjectKeyword: 'object',
+  TSUnknownKeyword: 'unknown',
+  TSSymbolKeyword: 'symbol',
+}
 
-export function transform(ast) {
-  switch (ast.type) {
-    case 'Program':
-      return transformProgram(ast)
-    case 'TSTypeAliasDeclaration':
-      return transformTypeAlias(ast)
-    case 'TSTypeLiteral':
-      return transformTypeLiteral(ast)
-    case 'TSPropertySignature':
-      return transformPropertySignature(ast)
-    case 'TSTypeAnnotation':
-      return transformTypeAnnotation(ast)
-    case 'TSStringKeyword':
-      return 'string'
-    case 'TSNumberKeyword':
-      return 'number'
-    default:
-      throw new Error(`Unknown ast type: ${ast.type}`)
+function isBasicType(type: string): boolean {
+  return !!BASIC_TYPES[type]
+}
+
+function transformBasicTypes(ast, transform: boolean) {
+  if (!transform) return ast
+
+  return {
+    type: 'MemberExpression',
+    object: { type: 'Identifier', name: 'TSRuntime' },
+    property: { type: 'Identifier', name: BASIC_TYPES[ast.type] },
+    computed: false,
+    optional: false
   }
 }
 
-function transformProgram(ast) {
+export function transform(ast, transformAll: boolean = false) {
+  switch (ast.type) {
+    case 'Program':
+      return transformProgram(ast, transformAll)
+    case 'TSTypeAliasDeclaration':
+      return transformTypeAlias(ast)
+    case 'TSTypeLiteral':
+      return transformTypeLiteral(ast, transformAll)
+    case 'TSPropertySignature':
+      return transformPropertySignature(ast, transformAll)
+    case 'TSTypeAnnotation':
+      return transformTypeAnnotation(ast, transformAll)
+    default:
+      if (isBasicType(ast.type))
+        return transformBasicTypes(ast, transformAll)
+      return ast
+  }
+}
+
+function transformProgram(ast, transformAll: boolean) {
   const node = Object.assign({}, ast)
-  node.body = ast.body.map(transform)
+  node.body = ast.body.map(ast => transform(ast, transformAll))
   return node
 }
 
@@ -36,11 +61,13 @@ function transformTypeAlias(ast) {
   return {
     type: 'VariableDeclaration',
     kind: 'const',
-    declarations: [{ id: ast.id, init: transform(ast.typeAnnotation) }],
+    declarations: [{ id: ast.id, init: transform(ast.typeAnnotation, true) }],
   }
 }
 
-function transformTypeLiteral(ast) {
+function transformTypeLiteral(ast, transformAll: boolean) {
+  if (!transformAll) return ast
+
   return {
     type: 'CallExpression',
     callee: {
@@ -48,11 +75,13 @@ function transformTypeLiteral(ast) {
       object: { type: 'Identifier', name: 'TSRuntime' },
       property: { type: 'Identifier', name: 'Object' },
     },
-    arguments: ast.members.map(transform)
+    arguments: ast.members.map(ast => transform(ast, transformAll))
   }
 }
 
-function transformPropertySignature(ast) {
+function transformPropertySignature(ast, transformAll: boolean) {
+  if (!transformAll) return ast
+
   return {
     type: 'CallExpression',
     callee: {
@@ -64,17 +93,19 @@ function transformPropertySignature(ast) {
     },
     arguments: [
       { type: 'Literal', value: ast.key.name, raw: JSON.stringify(ast.key.name) },
-      transform(ast.typeAnnotation),
+      transform(ast.typeAnnotation, transformAll),
     ],
     optional: false,
   }
 }
 
-function transformTypeAnnotation(ast) {
+function transformTypeAnnotation(ast, transformAll: boolean) {
+  if (!transformAll) return ast
+
   return {
     type: 'MemberExpression',
     object: { type: 'Identifier', name: 'TSRuntime' },
-    property: transform(ast.typeAnnotation),
+    property: transform(ast.typeAnnotation, transformAll),
     computed: false,
     optional: false,
   }
